a849e63e2dca305decac9be12b31d7f17a463199
Hwang Seok O <ohs6006@gmail.com>
2023-02-01 14:48:02 +0900

    투두 아이템 스타일링

--- src/components/Todo/Todo.jsx
+++ src/components/Todo/Todo.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { FaTrashAlt } from "react-icons/fa";
+import styles from "./Todo.module.css";
 
 export default function Todo({ todo, onUpdate, onDelete }) {
-  const { text, status } = todo;
+  const { text, status, id } = todo;
   const handleChange = e => {
     const status = e.target.checked ? "completed" : "active";
     onUpdate({ ...todo, status });
   };
   const handleDelete = () => onDelete(todo);
   return (
-    <li>
+    <li className={styles.todo}>
       <input
         type="checkbox"
-        name=""
-        id="checkbox"
+        id={id}
         checked={status === "completed"}
         onChange={handleChange}
+        className={styles.checkbox}
       />
-      <label htmlFor="checkbox">{text}</label>
-      <button onClick={handleDelete}>
-        <FaTrashAlt />
-      </button>
+      <label htmlFor={id} className={styles.text}>
+        {text}
+      </label>
+      <span className={styles.icon}>
+        <button onClick={handleDelete} className={styles.button}>
+          <FaTrashAlt />
+        </button>
+      </span>
     </li>
   );
 }
