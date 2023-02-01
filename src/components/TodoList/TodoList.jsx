6a44d5d5c4c52ecdd95500dfa12677a7cb2dafaf
Hwang Seok O <ohs6006@gmail.com>
2023-02-01 11:24:19 +0900

    투두 아이템 체크 & 삭제

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import AddTodo from "../AddTodo/AddTodo";
+import Todo from "../Todo/Todo";
 
 // 투두 에서는 전체적인 데이터를 보여주는 역할만 한다.
 
@@ -13,11 +14,26 @@ const TodoList = () => {
     // 새로운 투두를 todos에 업데이트 해야 함
     setTodos([...todos, todo]);
   };
+
+  const handleUpdate = updated => {
+    // 업데이트된 아이디와 동일하다면 업데이트로 바꿔줌
+    setTodos(todos.map(el => (el.id === updated.id ? updated : el)));
+  };
+  const handleDelete = deleted => {
+    // 삭제된 아이디를 제외한 배열을 리턴
+    setTodos(todos.filter(el => el.id !== deleted.id));
+  };
+
   return (
     <section>
       <ul>
         {todos.map(item => (
-          <li key={item.id}>{item.text}</li>
+          <Todo
+            key={item.id}
+            todo={item}
+            onUpdate={handleUpdate}
+            onDelete={handleDelete}
+          ></Todo>
         ))}
       </ul>
       <AddTodo onAdd={handleAdd} />
